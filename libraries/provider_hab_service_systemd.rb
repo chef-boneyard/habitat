c0e2fdda5b64c6efe4a4c39bf6480637190bf85c
jtimberman <joshua@chef.io>
2016-11-02 18:23:53 -0600

    Add hab_service resource
    This commit adds a hab_service resource, which is a thinly veiled
    wrapper around a minimal systemd_unit and service resource.
    
    Signed-off-by: Joshua Timberman <joshua@chef.io>

---
+++ libraries/provider_hab_service_systemd.rb
@@ -0,0 +1,86 @@
+# Copyright:: Copyright 2016, Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require "chef/provider/service"
+
+class Chef
+  class Provider
+    class Service
+      class HabServiceSystemd < Chef::Provider::Service
+
+        use_inline_resources
+
+        provides :hab_service_systemd
+        provides :hab_service do |node|
+          Chef::Platform::ServiceHelpers.service_resource_providers.include?(:systemd)
+        end
+
+        action :start do
+          declare_resource(:systemd_unit, "#{short_service_name}.service") do
+            content unit_content
+            action :create
+          end
+
+          declare_resource(:service, short_service_name) do
+            subscribes :restart, "systemd_unit[#{short_service_name}.service]"
+            action :start
+          end
+        end
+
+        action :stop do
+          declare_resource(:service, short_service_name) do
+            action :stop
+          end
+        end
+
+        action :enable do
+          declare_resource(:systemd_unit, "#{short_service_name}.service") do
+            content unit_content
+            action :create
+          end
+        end
+
+        def short_service_name
+          new_resource.service_name.split("/")[1]
+        end
+
+        def unit_content
+          return new_resource.unit_content if new_resource.unit_content
+          {
+            Unit: {
+              Description: short_service_name,
+              After: "network.target audit.service"
+            },
+              Service: {
+              Environment: new_resource.environment,
+              ExecStart: "/bin/hab start #{new_resource.service_name}",
+              Restart: "on-failure"
+            }
+          }
+        end
+
+        private
+
+        def hab(*command)
+          shell_out_with_systems_locale!(a_to_s("hab", *command))
+        rescue  Errno::ENOENT
+          Chef::Log.fatal("'hab' binary not found, use the 'hab_install' resource to install it first")
+          raise
+        end
+      end
+    end
+  end
+end
--- metadata.rb
+++ metadata.rb
@@ -12,3 +12,6 @@ end
 
 source_url "https://github.com/chef-cookbooks/habitat" if respond_to?(:source_url)
 issues_url "https://github.com/chef-cookbooks/habitat/issues" if respond_to?(:issues_url)
+
+# we need chef 12.5 for custom resources, and 12.11 for systemd_unit
+chef_version ">= 12.11"
