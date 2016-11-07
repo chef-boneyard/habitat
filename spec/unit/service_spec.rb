335f413126c85260b157fff3eb4f333e792fc0ec
jtimberman <joshua@chef.io>
2016-11-07 15:33:45 -0700

    Add specs!
    This commit adds ChefSpec pre-convergence for the `test` cookbook, and
    Inspec post-convergence for `kitchen verify`.
    
    Signed-off-by: Joshua Timberman <joshua@chef.io>

---
+++ spec/unit/service_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe 'test::service' do
+  cached(:chef_run) do
+    ChefSpec::ServerRunner.new(
+      platform: 'ubuntu',
+      version: '16.04'
+    ).converge(described_recipe)
+  end
+
+  context 'when compiling the service recipe' do
+    it 'creates a hab user' do
+      expect(chef_run).to create_user('hab')
+    end
+
+    it 'starts the core/nginx service' do
+      expect(chef_run).to start_hab_service('core/nginx')
+    end
+
+    it 'enables the core/redis service' do
+      expect(chef_run).to enable_hab_service('core/redis')
+    end
+  end
+end
