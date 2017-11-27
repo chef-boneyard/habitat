35bc02403286548772f6347bd977213f7a9f50ed
jtimberman <joshua@chef.io>
2017-11-27 09:29:56 -0700

    update for 0.39.1
    Signed-off-by: Joshua Timberman <joshua@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Habitat related resources for chef-client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.39.0'
+version '0.39.1'
 
 %w(ubuntu debian redhat centos suse scientific oracle amazon opensuse opensuseleap).each do |os|
   supports os
