53a39b75606025d988e6d6f06a0b053a1bb4ebf3
Jon Cowie <jonlives@gmail.com>
2019-01-24 16:07:40 +0000

    Update hab-launcher to 9421
    Signed-off-by: Jon Cowie <jonlives@gmail.com>

--- libraries/habitat_shared.rb
+++ libraries/habitat_shared.rb
@@ -16,8 +16,9 @@
 
 module Habitat
   module Shared
-    HAB_VERSION = '0.69.0'.freeze
-    LINUX_LAUNCHER_VERSION = '8659'.freeze
+
+    HAB_VERSION = '0.73.0'.freeze
+    LINUX_LAUNCHER_VERSION = '9421'.freeze
     WINDOWS_LAUNCHER_VERSION = '8656'.freeze
     WINDOWS_SERVICE_VERSION = '0.3.1'.freeze
 
