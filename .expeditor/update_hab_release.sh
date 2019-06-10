2ed023dfb8b3c5a9fb1c3df1788df613c8406b5d
Jon Cowie <jonlives@gmail.com>
2019-06-10 14:58:43 +0100

    Fix expeditor variable name
    Signed-off-by: Jon Cowie <jonlives@gmail.com>

--- .expeditor/update_hab_release.sh
+++ .expeditor/update_hab_release.sh
@@ -5,25 +5,25 @@
 #
 set -evx
 
-if [[ -z $VERSION ]]; then
+if [[ -z $EXPEDITOR_VERSION ]]; then
   echo "ERROR: Version undefined, cannot continue, exiting"
   exit 1
 fi
 
-branch="expeditor/${VERSION}"
+branch="expeditor/${EXPEDITOR_VERSION}"
 git checkout -b ${branch}
 
-sed -i "s/%r{^hab .*\/}/%r{^hab ${VERSION}\/}/" test/integration/config/default_spec.rb
-sed -i "s/%r{^hab .*\/}/%r{^hab ${VERSION}\/}/" test/integration/package/default_spec.rb
-sed -i "s/%r{^hab .*\/}/%r{^hab ${VERSION}\/}/" test/integration/user-toml/default_spec.rb
-sed -i "s/HAB_VERSION = '.*'.freeze/HAB_VERSION = '${VERSION}'.freeze/" libraries/habitat_shared.rb
-sed -i "s/^- Habitat version: .*/- Habitat version: ${VERSION}/" README.md
-sed -i "s/^version '.*'/version '${VERSION}'/" metadata.rb
+sed -i "s/%r{^hab .*\/}/%r{^hab ${EXPEDITOR_VERSION}\/}/" test/integration/config/default_spec.rb
+sed -i "s/%r{^hab .*\/}/%r{^hab ${EXPEDITOR_VERSION}\/}/" test/integration/package/default_spec.rb
+sed -i "s/%r{^hab .*\/}/%r{^hab ${EXPEDITOR_VERSION}\/}/" test/integration/user-toml/default_spec.rb
+sed -i "s/HAB_VERSION = '.*'.freeze/HAB_VERSION = '${EXPEDITOR_VERSION}'.freeze/" libraries/habitat_shared.rb
+sed -i "s/^- Habitat version: .*/- Habitat version: ${EXPEDITOR_VERSION}/" README.md
+sed -i "s/^version '.*'/version '${EXPEDITOR_VERSION}'/" metadata.rb
 
-echo "${VERSION}" > VERSION
+echo "${EXPEDITOR_VERSION}" > VERSION
 
 git add .
-git commit --message "Update to habitat $VERSION" --message "This pull request was triggered automatically via Expeditor when Habitat $VERSION was promoted to stable." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
+git commit --message "Update to habitat $EXPEDITOR_VERSION" --message "This pull request was triggered automatically via Expeditor when Habitat $EXPEDITOR_VERSION was promoted to stable." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
 
 . /helper_functions.sh
 open_pull_request
