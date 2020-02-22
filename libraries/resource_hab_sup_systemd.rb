ff575d7777debcecfa8a0b32180e27d64a38d908
Jeff Brimager <jbrimager@chef.io>
2020-02-22 12:42:22 -0600

    correction to systemd template
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- libraries/resource_hab_sup_systemd.rb
+++ libraries/resource_hab_sup_systemd.rb
@@ -39,7 +39,10 @@ class Chef
                     LimitNOFILE: (new_resource.limit_no_files if new_resource.limit_no_files),
                     Environment: service_environment,
                     ExecStart: "/bin/hab sup run #{exec_start_options}",
+                    ExecStop: '/bin/hab sup term',
                     Restart: 'on-failure',
+                    RestartSec: '10',
+                    KillMode: 'process',
                   }.compact,
                   Install: {
                     WantedBy: 'default.target',
