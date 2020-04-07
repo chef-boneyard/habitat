75bae1f8bd1e3a56f42ce7a0c362b654b2e73021
Jeff Brimager <jbrimager@chef.io>
2020-04-07 08:19:55 -0500

    correcting 1 final test
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- spec/unit/sup_spec.rb
+++ spec/unit/sup_spec.rb
@@ -89,7 +89,7 @@ describe 'test::sup' do
             },
             Service: {
               Environment: [],
-              ExecStart: '/bin/hab sup run --listen-gossip 0.0.0.0:7998 --listen-http 0.0.0.0:7999 --peer 127.0.0.2 --peer 127.0.0.3 --update-condition latest',
+              ExecStart: '/bin/hab sup run --listen-gossip 0.0.0.0:7998 --listen-http 0.0.0.0:7999 --peer 127.0.0.2 --peer 127.0.0.3',
               ExecStop: '/bin/hab sup term',
               Restart: 'on-failure',
             },
