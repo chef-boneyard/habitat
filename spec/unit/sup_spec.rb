328e90a5bd63d3a50da3ff01d7607aae1388b519
Jeff Brimager <jbrimager@chef.io>
2020-04-07 08:22:53 -0500

    missed 2 more tests correcting
    Signed-off-by: Jeff Brimager <jbrimager@chef.io>

--- spec/unit/sup_spec.rb
+++ spec/unit/sup_spec.rb
@@ -138,7 +138,7 @@ describe 'test::sup' do
           group: 'root',
           mode: '0644',
           variables: {
-            exec_start_options: '--listen-gossip 0.0.0.0:7998 --listen-http 0.0.0.0:7999 --peer 127.0.0.2 --peer 127.0.0.3 --update-condition latest',
+            exec_start_options: '--listen-gossip 0.0.0.0:7998 --listen-http 0.0.0.0:7999 --peer 127.0.0.2 --peer 127.0.0.3',
             auth_token: nil,
             gateway_auth_token: nil,
           }
@@ -184,7 +184,7 @@ describe 'test::sup' do
           mode: '0755',
           variables: {
             name: 'hab-sup',
-            exec_start_options: '--listen-gossip 0.0.0.0:7998 --listen-http 0.0.0.0:7999 --peer 127.0.0.2 --peer 127.0.0.3 --update-condition latest',
+            exec_start_options: '--listen-gossip 0.0.0.0:7998 --listen-http 0.0.0.0:7999 --peer 127.0.0.2 --peer 127.0.0.3',
             auth_token: nil,
             gateway_auth_token: nil,
           }
